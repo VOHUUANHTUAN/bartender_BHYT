4aba1f940601d025227074db7115b94960cb1ea1
Vang Mai Quyet <maiquyetvang2002@gmail.com>
2023-11-28 00:04:00 +0700

    fix wrong api name
    TaiKhoan -> User

--- frontend/bhtruongtho/src/api/connect.js
+++ frontend/bhtruongtho/src/api/connect.js
@@ -1,18 +1,17 @@
 import axiosClient from "./axiosClient";
 
 const END_POINT = {
-    GOIBAOHIEM: "GoiBaoHiem",
-    BOOK: "Book",
-    BOOKBYID: "Book/id:Guid",
-    TaiKhoan: "TaiKhoan",
-}
-export const getGoiBHAPI= () => {
-    return axiosClient.get(`${END_POINT.GOIBAOHIEM}`);
-}
+  GOIBAOHIEM: "GoiBaoHiem",
+  BOOK: "Book",
+  BOOKBYID: "Book/id:Guid",
+  TaiKhoan: "User",
+};
+export const getGoiBHAPI = () => {
+  return axiosClient.get(`${END_POINT.GOIBAOHIEM}`);
+};
 export const getBookAPI = () => {
-    return axiosClient.get(`${END_POINT.BOOK}`);
-}
+  return axiosClient.get(`${END_POINT.BOOK}`);
+};
 export const getTaiKhoanByUsername = (username) => {
-    return axiosClient.get(`${END_POINT.TaiKhoan}/${username}`);
-  };
-  
+  return axiosClient.get(`${END_POINT.TaiKhoan}/${username}`);
+};
