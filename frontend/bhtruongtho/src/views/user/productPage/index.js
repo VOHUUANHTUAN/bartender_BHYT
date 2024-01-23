b48a7df0893fbc60ed612d5d1fd5ca99e286dccd
minhTienn2602 <96586521+minhTienn2602@users.noreply.github.com>
2024-01-24 00:57:22 +0700

    sửa financialReport

--- frontend/bhtruongtho/src/views/user/productPage/index.js
+++ frontend/bhtruongtho/src/views/user/productPage/index.js
@@ -4,11 +4,13 @@ import "./style.scss";
 import { Link } from "react-router-dom";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
+
 const ProductPage = () => {
-    const [goiBHs, setgoiBHs] = useState([]);
+    const [goiBHs, setGoiBHs] = useState([]);
     const fetchData = async () => {
-        setgoiBHs(await getGoiBHAPI());
+        setGoiBHs(await getGoiBHAPI());
     };
+
     useEffect(() => {
         fetchData();
     }, []);
@@ -21,81 +23,71 @@ const ProductPage = () => {
 
         return formattedAmount;
     };
+
     const [currentPage, setCurrentPage] = useState(1);
-    const goiBHsPerPage = 6; // Số gói Bảo hiểm trên mỗi trang
-    // Tính toán vị trí bắt đầu và kết thúc của danh sách gói Bảo hiểm trên trang hiện tại
+    const goiBHsPerPage = 6;
+
     const startIndex = (currentPage - 1) * goiBHsPerPage;
     const endIndex = startIndex + goiBHsPerPage;
     const displayedGoiBHs = goiBHs.slice(startIndex, endIndex);
-    // Tính toán số trang dựa trên số lượng gói Bảo hiểm
     const totalPages = Math.ceil(goiBHs.length / goiBHsPerPage);
-    // Xử lý sự kiện thay đổi trang
+
     const handlePageChange = (event, newPage) => {
         setCurrentPage(newPage);
     };
 
     return (
-        <>
-            <div className="container__body">
-                <div className="productPage">
-                    <ul>
-                        <div className="cards__container">
-                            {displayedGoiBHs.map((goiBH, goiBHKey) => (
-                                <li
-                                    key={startIndex + goiBHKey}
-                                    className={`card__container ${
-                                        goiBHKey === 0 ? "active" : ""
-                                    }`}
-                                >
-                                    <div className="img-container">
-                                        <img src="" alt=""></img>
+        <div className="container__body">
+            <div className="productPage">
+                <ul>
+                    <div className="cards__container">
+                        {displayedGoiBHs.map((goiBH, goiBHKey) => (
+                            <li
+                                key={startIndex + goiBHKey}
+                                className={`card__container ${
+                                    goiBHKey === 0 ? "active" : ""
+                                }`}
+                            >
+                                <div className="img-container">
+                                    <img src="" alt=""></img>
+                                </div>
+                                <div className="card__content">
+                                    <div className="card__title">
+                                        <h3>{goiBH.tenGoiBH}</h3>
                                     </div>
-                                    <div className="card__content">
-                                        <div className="card__title">
-                                            <h3> {goiBH.tenGoiBH}</h3>
-                                        </div>
+                                    {goiBH && (
                                         <div className="card__body">
                                             <p>{goiBH.motaGoiBH}</p>
-                                            <p>Giá: {goiBH.gia} VND</p>
-                                            <p>
-                                                Tỉ lệ hoàn tiền:{" "}
-                                                {goiBH.tiLeHoanTien}%
-                                            </p>
+                                            <p>Giá: {formatCurrency(goiBH.gia)}</p>
+                                            <p>Tỉ lệ hoàn tiền: {goiBH.tiLeHoanTien}%</p>
                                         </div>
-                                    </div>
-
-                                    <div className="card__body">
-                                        <p>{goiBH.motaGoiBH}</p>
-                                        <p>Giá: {formatCurrency(goiBH.gia)}</p>
-                                        <p>Tỉ lệ hoàn tiền: {goiBH.tiLeHoanTien}%</p>
-
-                                    <div className="card__btn">
-                                        <Link to={`detail/${goiBH.maGoiBH}`}>
-                                            <p>Xem thêm</p>
-                                        </Link>
-
-                                    </div>
-                                </li>
-                            ))}
-                        </div>
-                    </ul>
-                    <Stack
-                        spacing={2}
-                        justifyContent="center"
-                        alignItems="center"
-                        style={{ margin: "20px" }}
-                    >
-                        <Pagination
-                            count={totalPages}
-                            page={currentPage}
-                            onChange={handlePageChange}
-                            variant="outlined"
-                            color="success"
-                        />
-                    </Stack>
-                </div>
+                                    )}
+                                </div>
+                                <div className="card__btn">
+                                    <Link to={`detail/${goiBH.maGoiBH}`}>
+                                        <p>Xem thêm</p>
+                                    </Link>
+                                </div>
+                            </li>
+                        ))}
+                    </div>
+                </ul>
+                <Stack
+                    spacing={2}
+                    justifyContent="center"
+                    alignItems="center"
+                    style={{ margin: "20px" }}
+                >
+                    <Pagination
+                        count={totalPages}
+                        page={currentPage}
+                        onChange={handlePageChange}
+                        variant="outlined"
+                        color="success"
+                    />
+                </Stack>
             </div>
-        </>
+        </div>
     );
 };
 
